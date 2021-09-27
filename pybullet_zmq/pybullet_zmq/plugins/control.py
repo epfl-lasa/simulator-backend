6e5212a3868970b1452480ef5fdcee99d706bcc4
Dominic Reber <dominic.reber@epfl.ch>
2021-09-27 16:36:06 +0200

    Change to usage of network_interfaces

--- pybullet_zmq/pybullet_zmq/plugins/control.py
+++ pybullet_zmq/pybullet_zmq/plugins/control.py
@@ -1,35 +1,46 @@
-from pybullet_simulation import network
+from network_interfaces.control_type import ControlType
+from network_interfaces.zmq import network
 
 
 class Control:
     def __init__(self, zmq_context, pybullet, robot, **kwargs):
         self._pb = pybullet
         self._robot = robot
-        self._subscriber = network.configure_subscriber(zmq_context, "0.0.0.0:" + str(kwargs["URI"]))
+        self._subscriber = network.configure_subscriber(zmq_context, str(kwargs["URI"]))
 
         self._control_params = {"bodyUniqueId": self._robot.id, "jointIndices": self._robot.joint_indices}
-        self._last_command_type = 0
-        self._force_commands = [100] * len(self._robot.joint_indices)
+        self._last_command_type = ControlType.UNDEFINED
+        self._force_commands = [100] * self._robot.nb_joints
 
     def execute(self):
-        """
-        Execute the plugin. This function is called from main update loop in the pybullet ros node.
-        """
-        command = network.poll_command(self._subscriber)
-        # TODO this currently only supports effort control
+        control_params = {"bodyUniqueId": self._robot.id, "jointIndices": self._robot.joint_indices}
+        command = network.receive_command(self._subscriber)
         if command:
-            if self._last_command_type != 3:
-                self._last_command_type = 3
-                self._pb.setJointMotorControlArray(self._robot.id, self._robot.joint_indices,
-                                                   self._pb.VELOCITY_CONTROL,
-                                                   forces=[0] * len(self._robot.joint_indices))
-            self._control_params["controlMode"] = self._pb.TORQUE_CONTROL
-            self._control_params["forces"] = self._robot.compensate_gravity(command.joint_state.get_torques().tolist())
-        elif self._last_command_type == 3 and not command:
-            self._last_command_type = 0
-            self._control_params["controlMode"] = self._pb.VELOCITY_CONTROL
-            self._control_params["targetVelocities"] = [0] * len(self._robot.joint_indices)
-            self._control_params["forces"] = self._force_commands
+            if len(set(command.control_type)) > 1:
+                raise ValueError("Different control types per joint are currently not allowed. "
+                                 "Make sure all the joints have the same control type.")
+            if command.control_type[0] == ControlType.POSITION.value:
+                self._last_command_type = ControlType.POSITION
+                control_params["controlMode"] = self._pb.POSITION_CONTROL
+                control_params["targetPositions"] = command.joint_state.get_positions()
+                control_params["forces"] = self._force_commands
+            elif command.control_type[0] == ControlType.VELOCITY.value:
+                self._last_command_type = ControlType.VELOCITY
+                control_params["controlMode"] = self._pb.VELOCITY_CONTROL
+                control_params["targetVelocities"] = command.joint_state.get_velocities()
+                control_params["forces"] = self._force_commands
+            elif command.control_type[0] == ControlType.EFFORT.value:
+                if self._last_command_type != ControlType.EFFORT:
+                    self._last_command_type = ControlType.EFFORT
+                    self._pb.setJointMotorControlArray(self._robot.id, self._robot.joint_indices,
+                                                       self._pb.VELOCITY_CONTROL,
+                                                       forces=[0] * self._robot.nb_joints)
+                control_params["controlMode"] = self._pb.TORQUE_CONTROL
+                control_params["forces"] = self._robot.compensate_gravity(command.joint_state.get_torques())
 
-        if "controlMode" in self._control_params.keys():
-            self._pb.setJointMotorControlArray(**self._control_params)
+        if not command and self._last_command_type == ControlType.EFFORT:
+            control_params["controlMode"] = self._pb.TORQUE_CONTROL
+            control_params["forces"] = self._robot.compensate_gravity([0.0] * self._robot.nb_joints)
+
+        if "controlMode" in control_params.keys():
+            self._pb.setJointMotorControlArray(**control_params)
--- pybullet_zmq/pybullet_zmq/plugins/robot_state_publisher.py
+++ pybullet_zmq/pybullet_zmq/plugins/robot_state_publisher.py
@@ -1,12 +1,16 @@
-from pybullet_simulation import network
+from network_interfaces.zmq import network
+from state_representation import Parameter, StateType
 
 
 class RobotStatePublisher:
     def __init__(self, zmq_context, pybullet, robot, **kwargs):
         self._pb = pybullet
         self._robot = robot
-        self._publisher = network.configure_publisher(zmq_context, "*:" + str(kwargs["URI"]))
+        self._publisher = network.configure_publisher(zmq_context, str(kwargs["URI"]))
 
     def execute(self):
-        state = network.StateMessage(self._robot.get_ee_link_state(), self._robot.get_joint_state())
+        joint_state = self._robot.get_joint_state()
+        mass = Parameter("mass", self._robot.get_inertia(joint_state.get_positions()), StateType.PARAMETER_MATRIX)
+        state = network.StateMessage(self._robot.get_ee_link_state(), joint_state,
+                                     self._robot.get_jacobian(joint_state.get_positions()), mass)
         network.send_state(state, self._publisher)
