45566330c1a8cb0623e39e2281ed971ee05e3401
Dominic Reber <dominic.reber@epfl.ch>
2021-09-28 13:33:57 +0200

    Specify names of state

--- pybullet_zmq/pybullet_zmq/plugins/robot_state_publisher.py
+++ pybullet_zmq/pybullet_zmq/plugins/robot_state_publisher.py
@@ -10,7 +10,8 @@ class RobotStatePublisher:
 
     def execute(self):
         joint_state = self._robot.get_joint_state()
-        mass = Parameter("mass", self._robot.get_inertia(joint_state.get_positions()), StateType.PARAMETER_MATRIX)
+        mass = Parameter(self._robot.name + "_mass", self._robot.get_inertia(joint_state.get_positions()),
+                         StateType.PARAMETER_MATRIX)
         state = network.StateMessage(self._robot.get_ee_link_state(), joint_state,
                                      self._robot.get_jacobian(joint_state.get_positions()), mass)
         network.send_state(state, self._publisher)
