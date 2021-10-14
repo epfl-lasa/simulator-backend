4ed0c03b11e414b0209c8450684775f5ffa503f4
Dominic Reber <dominic.reber@epfl.ch>
2021-10-14 13:26:14 +0200

    Set torques in torque control manually

--- pybullet_simulation/pybullet_simulation/robot.py
+++ pybullet_simulation/pybullet_simulation/robot.py
@@ -11,12 +11,14 @@ class Robot(RobotDescription):
     This is a collection of methods that gather 'real-time' information about the robot state and apply joint commands
     to the robot. In other words, those are the methods called (at each time step) in the simulation loop.
     Available methods (for usage, see documentation at function definition):
+        - set_torque_control
         - get_joint_state
         - get_link_state
         - get_ee_link_state
         - get_base_state
         - get_jacobian
         - get_inertia
+        - set_applied_motor_torques
         - compensate_gravity
     """
 
@@ -52,9 +54,32 @@ class Robot(RobotDescription):
         self._sim_uid = sim_uid
         super().__init__(self._sim_uid, name, urdf_path, fixed_base, use_inertia_from_file)
 
+        self._torque_controlled = False
+        self._applied_motor_torques = [0] * self.nb_joints
+
         self._log_info = log_info
         self._log_warn = log_warn
         self._log_err = log_err
+
+    def set_torque_control(self, enable):
+        """
+        Set torque control of the robot to true or false.
+
+        :param enable: Flag to set if robot is in torque control
+        :type enable: bool
+        """
+        assert isinstance(enable, bool), "[Robot::set_torque_control] Argument 'enable' has an incorrect type."
+        self._torque_controlled = enable
+
+    @property
+    def is_torque_controlled(self):
+        """
+        Check if robot is currently in torque control mode.
+
+        :return: Boolean if robot is in torque control
+        :rtype: bool
+        """
+        return self._torque_controlled
 
     def get_joint_state(self):
         """
@@ -67,12 +92,14 @@ class Robot(RobotDescription):
         joint_velocities = []
         joint_efforts = []
 
-        for idx in self.joint_indices:
-            joint_state = pb.getJointState(
-                self._id, idx, physicsClientId=self._sim_uid)
+        for j, idx in enumerate(self.joint_indices):
+            joint_state = pb.getJointState(self._id, idx, physicsClientId=self._sim_uid)
             joint_positions.append(joint_state[0])
             joint_velocities.append(joint_state[1])
-            joint_efforts.append(joint_state[3])
+            if self._torque_controlled:
+                joint_efforts.append(self._applied_motor_torques[j])
+            else:
+                joint_efforts.append(joint_state[3])
         state = JointState(self.name, self.joint_names)
         state.set_positions(joint_positions)
         state.set_velocities(joint_velocities)
@@ -169,6 +196,18 @@ class Robot(RobotDescription):
             return False
         else:
             return np.array(pb.calculateMassMatrix(self._id, joint_positions.tolist()))
+
+    def set_applied_motor_torques(self, applied_motor_torques):
+        """
+        Set the applied joint motor torques manually in torque control.
+
+        :param applied_motor_torques: The applied joint motor torques
+        :type applied_motor_torques: list of float
+        """
+        if len(applied_motor_torques) is not self.nb_joints:
+            self._log_warn("[Robot::set_applied_motor_torques] Invalid number of elements in your input.")
+            return
+        self._applied_motor_torques = applied_motor_torques
 
     def compensate_gravity(self, raw_command):
         """
